8968db78664722e737f5fcc07e72a2d8e7beadde
Alejandro Ajuria <alejoajuria@gmail.com>
2021-10-21 00:40:07 -0600

    Routes creadas
    rutas creadas de ordenProductos, Productos, y presentaciones

---
+++ src/routes/ordenProductos/index.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const router = Router();
+const controladorOrdenProducto = require('../../controllers/controladorOrdenProducto');
+router.get('/', controladorOrdenProducto.listarOrdenProducto);
+router.post('/', controladorOrdenProducto.GuardarOrdenProducto);
+router.delete('/', controladorOrdenProducto.EliminarQueryOrdenProducto);
+router.put('/', controladorOrdenProducto.ActualizarQueryOrdenProducto);
+module.exports = router;
