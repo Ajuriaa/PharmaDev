5c606497d8e5cd6e740cd66f6af546605dc46ee8
Lener Quiroz <55760347+LNer22@users.noreply.github.com>
2021-10-22 00:47:53 -0600

    Update index.js

--- src/routes/usuarios/index.js
+++ src/routes/usuarios/index.js
@@ -1,39 +1,67 @@
-const {
-    Router
-} = require('express');
+const {Router} = require('express');
 const controladorUsuarios = require('../../controllers/controladorUsuarios');
-const {
-    body
-} = require('express-validator');
+const {body} = require('express-validator');
 const router = Router();
 router.get('/', controladorUsuarios.listarUsuarios);
 router.post('/',
+    body('usuarioId').isLength({
+        min: 13,
+        max: 13
+    })
+    .withMessage('El numero de identidad debe contener 13 numeros')
+    .matches(/^[+]?([0-9][0-9]*(?:[\.][0-9]*)?|0*\.0*[0-9][0-9]*)(?:[eE][+-][0-9]+)?$/)
+    .withMessage('Debe ingresar solo numeros en el identificador de usuario'),
     body('usuarioCorreo').isEmail()
     .withMessage('Debe ingresar un correo electronico valido'),
     body('usuarioNombre').isLength({
-        min: 3
+        min: 5
     })
-    .withMessage('La longitud minima del nombre es de 3 caracteres'),
+    .withMessage('La longitud minima del nombre es de 5+ caracteres'),
     body('usuarioContrasena').isLength({
         min: 5
     })
     .withMessage('La longitud minima de la contraseña es de 5+ caracteres')
     .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/)
     .withMessage('La contraseña debe contener mayusculas minusculas y un numero'),
+    body('usuarioTelefono').isLength({
+        min: 8,
+        min: 8
+    })
+    .withMessage('El numero de telefono debe contener 8 caracteres')
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/)
+    .withMessage('El numero de telefono solo debe contener numeros'),
+    body('usuarioFechaNacimiento').isEmpty()
+    .withMessage('Debe ingresar su fecha de nacimiento'),
     controladorUsuarios.Guardar);
+
+
 router.delete('/', controladorUsuarios.EliminarQuery);
 router.put('/',
+    body('usuarioId').isLength({
+        min: 13,
+        max: 13
+    })
+    .withMessage('El numero de identidad debe contener 13 numeros')
+    .matches(/^[+]?([0-9][0-9]*(?:[\.][0-9]*)?|0*\.0*[0-9][0-9]*)(?:[eE][+-][0-9]+)?$/)
+    .withMessage('Debe ingresar solo numeros en el identificador de usuario'),
     body('usuarioCorreo').isEmail()
     .withMessage('Debe ingresar un correo electronico valido'),
     body('usuarioNombre').isLength({
-        min: 3
+        min: 5
     })
-    .withMessage('La longitud minima del nombre es de 3 caracteres'),
+    .withMessage('La longitud minima del nombre es de 5+ caracteres'),
     body('usuarioContrasena').isLength({
         min: 5
     })
     .withMessage('La longitud minima de la contraseña es de 5+ caracteres')
     .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/)
     .withMessage('La contraseña debe contener mayusculas minusculas y un numero'),
+    body('usuarioTelefono').isLength({
+        min: 8,
+        min: 8
+    })
+    .withMessage('El numero de telefono debe contener 8 caracteres')
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/)
+    .withMessage('El numero de telefono solo debe contener numeros'),
     controladorUsuarios.ActualizarQuery);
 module.exports = router;
