5ea85086bc0d3beefdf7f3dd6a6fa5f270f7c04c
Alejandro Ajuria <alejoajuria@gmail.com>
2021-10-22 22:25:09 -0600

    Validacion de Rutas añadidas
    Rutas de productos, OrdenProductos y Presentacion

--- src/routes/ordenProductos/index.js
+++ src/routes/ordenProductos/index.js
@@ -1,8 +1,22 @@
 const { Router } = require('express')
-const router = Router()
 const controladorOrdenProducto = require('../../controllers/controladorOrdenProducto')
+const controladorAutenticacion = require('../../controllers/controladorAutenticacion')
+const { body } = require('express-validator')
+const router = Router()
 router.get('/', controladorOrdenProducto.listarOrdenProducto)
-router.post('/', controladorOrdenProducto.GuardarOrdenProducto)
+router.post('/',
+    body('productoId').isInt().withMessage('Ingrese una id de producto valida'),
+    body('ordenId').isInt().withMessage('Ingrese una id de orden valida'),
+    body('ordenProductoCantidad').notEmpty().isNumeric().withMessage('Este campo no puede estar vacio')
+    .matches('^[0-9]*$').withMessage('Este Campo debe ser un numero'),
+    controladorAutenticacion.validarAutenticado,
+    controladorOrdenProducto.GuardarOrdenProducto)
 router.delete('/', controladorOrdenProducto.EliminarQueryOrdenProducto)
-router.put('/', controladorOrdenProducto.ActualizarQueryOrdenProducto)
+router.put('/',
+    body('productoId').isInt().withMessage('Ingrese una id de producto valida'),
+    body('ordenId').isInt().withMessage('Ingrese una id de orden valida'),
+    body('ordenProductoCantidad').notEmpty().isNumeric().withMessage('Este campo no puede estar vacio')
+    .matches('^[0-9]*$').withMessage('Este Campo debe ser un numero'),
+    controladorAutenticacion.validarAutenticado,
+    controladorOrdenProducto.ActualizarQueryOrdenProducto)
 module.exports = router
--- src/routes/productos/index.js
+++ src/routes/productos/index.js
@@ -1,8 +1,44 @@
 const { Router } = require('express')
-const router = Router()
 const controladorProducto = require('../../controllers/controladorProducto')
+const controladorAutenticacion = require('../../controllers/controladorAutenticacion')
+const { body } = require('express-validator')
+const router = Router()
 router.get('/', controladorProducto.listarProductos)
-router.post('/', controladorProducto.GuardarProducto)
+router.post('/',
+    body('productoNombre').isLength({
+        min: 3, max: 25
+    })
+    .withMessage('La longitud minima del nombre es de 3 caracteres y maximo de 25'),
+    body('productoDescripcion').isLength({
+        min: 5, max: 250
+    })
+    .withMessage('Inserte la descripcion del producto con mas de 5 caracteres y menos de 250'),
+    body('productoPrecio').isFloat({
+        min: 1
+    })
+    .withMessage('El valor del producto debe ser mayor que 0'),
+    body('productoFechaCreado').isEmpty().withMessage('Este campo no puede estar vacio'),
+    body('laboratorioId').isInt().withMessage('Ingrese una id de laboratorio valida'),
+    body('presentacionId').isInt().withMessage('Este campo no puede estar vacio'),
+    controladorAutenticacion.validarAutenticado,
+    controladorProducto.GuardarProducto)
 router.delete('/', controladorProducto.EliminarQueryProducto)
-router.put('/', controladorProducto.ActualizarQueryProducto)
+router.put('/',
+    body('productoNombre').isLength({
+        min: 3, max: 25
+    })
+    .withMessage('La longitud minima del nombre es de 3 caracteres y maximo de 25'),
+    body('productoDescripcion').isLength({
+        min: 5, max: 250
+    })
+    .withMessage('Inserte la descripcion del producto con mas de 5 caracteres y menos de 250'),
+    body('productoPrecio').isFloat({
+        min: 1
+    })
+    .withMessage('El valor del producto debe ser mayor que 0'),
+    body('productoFechaCreado').isEmpty().withMessage('Este campo no puede estar vacio'),
+    body('laboratorioId').isInt().withMessage('Ingrese una id de laboratorio valida'),
+    body('presentacionId').isInt().withMessage('Este campo no puede estar vacio'),
+    controladorAutenticacion.validarAutenticado,
+    controladorProducto.ActualizarQueryProducto)
 module.exports = router
