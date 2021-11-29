a12d9439a0978d3601ce3b18e43d92baa16696c6
Lener Quiroz <55760347+LNer22@users.noreply.github.com>
2021-11-29 13:17:42 -0600

    no recuerdo xd

---
+++ controllers/archivos.js
@@ -0,0 +1,24 @@
+const { validationResult } = require('express-validator')
+const fileUpload = require('express-fileupload');
+const msj = require('../components/mensaje')
+// Subir imagen de usuario
+exports.UploadImg = async (req, res) => {
+    const validacion = validationResult(req)
+    if (!validacion.isEmpty()) {
+        msj("Los datos ingresados no son validos", 500, validacion.array(), res)
+    } else {
+        let sampleFile;
+        let uploadPath;
+        if (!req.files || Object.keys(req.files).length === 0) {
+            return res.send(msj("No subió ningún archivo", 500, "", res));
+        }
+        sampleFile = req.files.sampleFile;
+        sampleFile.name = req.body.Id + '.png' || "error.png"
+        uploadPath = process.cwd() + '/public/users/' + sampleFile.name;
+        sampleFile.mv(uploadPath, function (err) {
+            if (err)
+                return res.send(msj("Ocurrió un error", 500, err, res));
+            res.send(msj("Archivo subido", 200, { uri: 'http:192.168.0.2:7777/users/' + sampleFile.name }, res));
+        });
+    }
+}
